b6cdf6fe81c447df49af3113612583d2c7fa14c1
Kier4n <kieran.boggs@imm-x1.iej-eleves.local>
2017-05-09 19:12:50 +0200

    Terminé Kieran Boggs

--- server/Api.js
+++ server/Api.js
@@ -5,14 +5,15 @@ const mongoose = require('mongoose');
 
 const UserController = require('./controller/User');
 const ProjectController = require('./controller/Project');
+const GradeController = require('./controller/Grade');
 
 const bodyParser = require('body-parser');
 const router = Router();
 
 if(process.env.NODE_ENV == 'production') {
-  mongoose.connect(`mongodb://${process.env.MLAB_DBUSER}:${process.env.MLAB_DBPASSWORD}@${process.env.MLAB_DBURL}`);
+  mongoose.connect('');
 } else {
-  mongoose.connect('mongodb://localhost:27017/kickass');
+  mongoose.connect('');
 }
 
 router.use(bodyParser.json({'extended': true}));
@@ -34,5 +35,9 @@ router.post('/project', ProjectController.createProject);
 router.put('/project/:id', ProjectController.updateProject);
 router.delete('/project/:id', ProjectController.deleteProject);
 router.get('/user/:id/projects', ProjectController.getUserProjects);
+
+router.post('/grade', GradeController.createGrade);
+router.put('/grade/:id', GradeController.updateGrade);
+router.delete('/grade/:id', GradeController.deleteGrade);
 
 module.exports = router;
---
+++ server/controller/Grade.js
@@ -0,0 +1,87 @@
+const Project = require('../model/Project');
+ const User = require('../model/User');
+ const Grade = require('../model/Grade');
+
+ const GradeController = {
+     createGrade: (req, res) {
+         Project.findOne({_id: req.params.projectId}).exec((err, project) => {
+             if (err) { return res.send(err); }
+             if (null == project) { return res.send('No project')}
+
+
+             User.findOne({_id: req.params.userId}).exec((err, user) => {
+                if (err) { return res.send(err); }
+                 if (null == user) { return res.send('No user')}
+
+                 Grade.findOne({_user: user, _project: project}).exec((err, grade) =>  {
+                     if (err) { return res.send(err); }
+                     if (null != grade) { return res.send('Already have a grade')}
+
+                     var grade = new Grade();
+                     grade._user = user;
+                     grade._project = project;
+                     grade.grade = req.body.grade;
+                     grade.date_created = Date.now();
+
+                     grade.save((err) => {
+                             if (err) {
+                                 return res.send(err);
+                             }
+                             res.json({message: `grade created `});
+                     });
+                 });
+
+             });
+
+
+         });
+     },
+     deleteGrade: (req, res) {
+         Grade.findByIdAndRemove(req.params.id, (err, grade) => {
+             if (err) {
+                 return res.send(err).status(500);
+             }
+             let str = (grade) ? `Project ${grade._id} deleted !` : 'No project were deleted.'
+             res.json({ message: str })
+         });
+     },
+
+     updateGrade: (req, res) {
+         Grade.findOne({_id: req.params.id}).exec((err, grade) =>  {
+             if (err) { return res.send(err); }
+             if (null == grade) { return res.send('no grade')}
+
+             grade.grade = req.body.grade;
+             grade.date_modified = Date.now();
+
+             grade.save((err) => {
+                 if (err) {
+                     return res.send(err);
+                 }
+                 res.json({message: `grade updated `});
+             });
+         });
+     },
+
+
+    projectsGrades: (req, res) {
+         Project.find().populate({
+            path: 'grades',
+             model: 'Grade'
+         }).exec((err, grade) => {
+             if (err) { return res.send(err); }
+
+
+         });
+     },
+
+     userProjects: (req, res) {
+
+     },
+
+     projectUsers: (req, res) {
+
+     }
+ }
+
+ module.exports = GradeController;
